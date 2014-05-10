2d125743d39985be1b7f0a3f18c5e72fde7c1c0e
Matt Parker <mdparker89@gmail.com>
2014-05-10 14:34:38 -0400

    Add DiffWordsToChars

--- diffmatchpatch/dmp.go
+++ diffmatchpatch/dmp.go
@@ -511,6 +511,20 @@ func (dmp *DiffMatchPatch) DiffLinesToChars(text1, text2 string) (string, string
 	return chars1, chars2, lineArray
 }
 
+// DiffLinesToChars split two texts into a list of strings.  Reduces the texts to a string of
+// hashes where each Unicode character represents one line.
+func (dmp *DiffMatchPatch) DiffWordsToChars(text1, text2 string) (string, string, []string) {
+	// '\x00' is a valid character, but various debuggers don't like it.
+	// So we'll insert a junk entry to avoid generating a null character.
+	lineArray := []string{""}    // e.g. lineArray[4] == 'Hello\n'
+	lineHash := map[string]int{} // e.g. lineHash['Hello\n'] == 4
+
+	chars1 := dmp.diffWordsToCharsMunge(text1, &lineArray, lineHash)
+	chars2 := dmp.diffWordsToCharsMunge(text2, &lineArray, lineHash)
+
+	return chars1, chars2, lineArray
+}
+
 // diffLinesToCharsMunge splits a text into an array of strings.  Reduces the texts to a string of
 // hashes where each Unicode character represents one line.
 // Modifies linearray and linehash through being a closure.
@@ -524,6 +538,46 @@ func (dmp *DiffMatchPatch) diffLinesToCharsMunge(text string, lineArray *[]strin
 
 	for lineEnd < len(text)-1 {
 		lineEnd = indexOf(text, "\n", lineStart)
+
+		if lineEnd == -1 {
+			lineEnd = len(text) - 1
+		}
+
+		line := text[lineStart : lineEnd+1]
+		lineStart = lineEnd + 1
+		lineValue_, ok := lineHash[line]
+
+		if ok {
+			runes = append(runes, rune(lineValue_))
+		} else {
+			*lineArray = append(*lineArray, line)
+			lineHash[line] = len(*lineArray) - 1
+			runes = append(runes, rune(len(*lineArray)-1))
+		}
+	}
+
+	return string(runes)
+}
+
+// diffLinesToCharsMunge splits a text into an array of strings.  Reduces the texts to a string of
+// hashes where each Unicode character represents one line.
+// Modifies linearray and linehash through being a closure.
+func (dmp *DiffMatchPatch) diffWordsToCharsMunge(text string, lineArray *[]string, lineHash map[string]int) string {
+	// Walk the text, pulling out a substring for each line.
+	// text.split('\n') would would temporarily double our memory footprint.
+	// Modifying text would create many large strings to garbage collect.
+	lineStart := 0
+	lineEnd := -1
+	runes := []rune{}
+
+	for lineEnd < len(text)-1 {
+		lineIndex := indexOf(text, "\n", lineStart)
+		wordIndex := indexOf(text, " ", lineStart)
+		if lineIndex < wordIndex && lineIndex != -1 || wordIndex == -1 {
+			lineEnd = lineIndex
+		} else {
+			lineEnd = wordIndex
+		}
 
 		if lineEnd == -1 {
 			lineEnd = len(text) - 1
